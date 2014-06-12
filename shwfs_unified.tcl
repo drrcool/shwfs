00a4a09d6c6756e48d30c731a66530bf3230ea7e
rcool <rcool@mmto.org>
2014-06-11 20:09:40 -0700

    Fixed minor bug with maestro focus

--- shwfs_unified.tcl
+++ shwfs_unified.tcl
@@ -182,7 +182,7 @@ set ref_foc(MegaCam) -468
 set ref_foc(Hecto) -2810
 #set ref_foc(Maestro) -2810
 #set ref_foc(Maestro) -1920 #Dec 2013
-set ref_foc(Maestro) -2620 #June 2014
+set ref_foc(Maestro) -2620 
 set ref_foc(SWIRC) -2017
 set ref_foc(MMIRS) 612
 
